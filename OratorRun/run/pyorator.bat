fc450903226ee90e9fbcb1bb392ad1b8c0912605
NewEconomicPolicy <mikemartin@ifb.co.uk>
2020-12-05 12:09:34 +0000

    removed management, run and setup directories

--- OratorRun/run/pyorator.bat
+++
@@ -1,6 +0,0 @@
-rem 
-@set PYTHONPATH=G:\PyOraDev\testPyOra\BioModels;G:\PyOraDev\testPyOra\EnvModelModules
-@set initial_working_dir=%cd%
-@chdir /D G:\PyOraDev\testPyOra\OratorRun\setup
-start cmd.exe /k "E:\Python38\python.exe -W ignore G:\PyOraDev\testPyOra\InitInptsRslts\PyOratorGUI.py"
-@chdir /D %initial_working_dir%
