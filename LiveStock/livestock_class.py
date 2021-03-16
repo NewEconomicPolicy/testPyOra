0acd65322ca88c9c6fb71af18b5946807447e1be
davidmcbey <davidmcbey@hotmail.co.uk>
2021-03-16 13:21:45 +0000

    Changes to merge data

--- LiveStock/livestock_class.py
+++ LiveStock/livestock_class.py
@@ -37,6 +37,12 @@ class Livestock:
             feeds = livestock.feeds
             print (f'There are {number} {livestock_name}')
 
+        # Calculate change inb production for each sub-area or management type, and each calculation method (i.e.
+        # N limitation, Zaks, Miami)
+
+
+
+
 
 
 
