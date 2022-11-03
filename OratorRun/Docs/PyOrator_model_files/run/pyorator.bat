126b986c5e6821f3decbccbfe67e40cf6d0ec4a8
Arkaned <arkan.lomas@gmail.com>
2022-11-03 19:11:37 +0000

    modify .bat and .json run and set up scripts as well as a variety of folders to become independent from external resources. (more work will need to be done to improve this attempt). Some variables still not detected when running...

--- OratorRun/Docs/PyOrator_model_files/run/pyorator.bat
+++ OratorRun/Docs/PyOrator_model_files/run/pyorator.bat
@@ -1,8 +1,11 @@
 rem 
-@set PYTHONPATH=D:\PyOraDevClone\testPyOra\BioModels;D:\PyOraDevClone\testPyOra\EnvModelModules;D:\PyOraDevClone\testPyOra\Constructor;D:\PyOraDevClone\testPyOra\LiveStock
+@set PYTHONPATH=D:\testPyOra\BioModels;D:\testPyOra\EnvModelModules;D:\testPyOra\Constructor;D:\testPyOra\LiveStock
 @set initial_working_dir=%cd%
-@chdir /D D:\PyOraDevClone\testPyOra\OratorRun\Docs\PyOrator_model_files\setup
-rem start cmd.exe /k "D:\PyOraDevClone\testPyOra\InitInptsRslts\PyOratorGUI.py"
-start cmd.exe /k "C:\Users\u07ad20\AppData\Local\Programs\Python\Python310\python.exe -W ignore D:\PyOraDevClone\testPyOra\InitInptsRslts\PyOratorGUI.py"
+@chdir /D D:\testPyOra\OratorRun\Docs\PyOrator_model_files\setup
+rem start cmd.exe /k "D:\testPyOra\InitInptsRslts\PyOratorGUI.py"
+
+rem file below needs to obscure C:/ path that contains my personal information (look into this soon!)
+rem also find out more about how double backslashes work. terminology a bit complicated for me brain atm...
+start cmd.exe /k "C:\Users\u07ad20\AppData\Local\Programs\Python\Python310\python.exe -W ignore D:\testPyOra\InitInptsRslts\PyOratorGUI.py"
 @chdir /D %initial_working_dir%
 
